d7d6c152c7a6d209d59380c1efba2eab0e19e4a0
Harim Seong <hseong@student.42seoul.kr>
2023-08-26 21:39:02 +0900

    image-gallery update

--- javascript/2_test/gallery-start/main.js
+++ javascript/2_test/gallery-start/main.js
@@ -5,14 +5,18 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const images_alt = ["blue eye", "stone", "flower", "egyptian wall painting", "butterfly"];
 
 /* Looping through images */
+for (let index = 0; index < images.length; ++index) {
+	const imgElem = document.createElement("img");
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+	imgElem.setAttribute('src', images[index]);
+	imgElem.setAttribute('alt', images_alt[index]);
+	thumbBar.appendChild(imgElem);
+}
 
 /* Wiring up the Darken/Lighten button */
